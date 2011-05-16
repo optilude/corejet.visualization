c713aaabc8f19bbbe380e2120b17c37070adac1d
Martin Aspeli <optilude@gmail.com>
2011-05-16 20:29:56 +0100

    Use lxml to apply xslt

--- corejet/visualization/generator.py
+++ corejet/visualization/generator.py
@@ -1,4 +1,3 @@
-import tempfile
 import os
 import os.path
 import shutil
@@ -21,37 +20,18 @@ def generateReportFromCatalogue(catalogue, directory):
             shutil.copytree(path, os.path.join(directory, os.path.basename(path)))
         else:
             shutil.copy(path, directory)
-
-    # XXX: This doesn't work - the XSLT returns None. Using fragile command
-    # line option instead :(
     
-    fileno, catalogueFile = tempfile.mkstemp()
+    # Load XSLT
+    xslt_tree = None
     
-    try:
-        with open(catalogueFile, 'w') as f:
-            catalogue.write(f)
+    with pkg_resources.resource_stream('corejet.visualization', os.path.join('xslt', 'corejet-to-jit.xsl')) as stream:
+        xslt_tree = lxml.etree.parse(stream)
     
-        os.system("xsltproc -o '%s' '%s' '%s'" % (
-                os.path.join(directory, 'corejet-requirements.js'),
-                pkg_resources.resource_filename('corejet.visualization',
-                        os.path.join('xslt', 'corejet-to-jit.xsl',)
-                    ),
-                catalogueFile,
-            ))
-    finally:
-        os.unlink(catalogueFile)
+    xslt = lxml.etree.XSLT(xslt_tree)
     
-    # # Load XSLT
-    # xslt_tree = None
-    # 
-    # with pkg_resources.resource_stream('corejet.visualization', os.path.join('xslt', 'corejet-to-jit.xsl')) as stream:
-    #     xslt_tree = lxml.etree.parse(stream)
-    # 
-    # xslt = lxml.etree.XSLT(xslt_tree)
-    # 
-    # # Create JIT output using XSLT
-    # source_tree = catalogue.serialize()
-    # target_tree = xslt(source_tree)
-    # 
-    # with open(os.path.join(directory, 'corejet-requirements.js'), 'w') as output:
-    #     target_tree.write(output)
+    # Create JIT output using XSLT
+    source_tree = catalogue.serialize()
+    target_tree = xslt(source_tree)
+    
+    with open(os.path.join(directory, 'corejet-requirements.js'), 'w') as output:
+        output.write(unicode(target_tree))
